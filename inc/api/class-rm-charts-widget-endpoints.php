0c5cbe733e52bfd7a6bb05c3570266ff0debdd7a
Sadanand Lonari <lonari.sadanand@gmail.com>
2024-05-16 01:24:47 +0530

    chor: Fix lint

--- inc/api/class-rm-charts-widget-endpoints.php
+++ inc/api/class-rm-charts-widget-endpoints.php
@@ -67,37 +67,38 @@ class RM_Charts_Widget_Endpoints {
 	/**
 	 * Fetch data
 	 *
+	 * @param mixed $request request data.
 	 * @since 1.0.0
 	 */
 	public function get_data( $request ) {
 		$filter = $request->get_param( 'filter' );
 		$data   = array();
 
-		// Define start and end dates based on the filter
-		$end_date = date( 'Y-m-d' );
+		// Define start and end dates based on the filter.
+		$end_date = gmdate( 'Y-m-d' );
 		switch ( $filter ) {
 			case '7days':
-				$start_date = date( 'Y-m-d', strtotime( '-7 days' ) );
+				$start_date = gmdate( 'Y-m-d', strtotime( '-7 days' ) );
 				break;
 			case '15days':
-				$start_date = date( 'Y-m-d', strtotime( '-15 days' ) );
+				$start_date = gmdate( 'Y-m-d', strtotime( '-15 days' ) );
 				break;
 			case '1month':
-				$start_date = date( 'Y-m-d', strtotime( '-1 month' ) );
+				$start_date = gmdate( 'Y-m-d', strtotime( '-1 month' ) );
 				break;
 			default:
-				$start_date = date( 'Y-m-d', strtotime( '-7 days' ) );
+				$start_date = gmdate( 'Y-m-d', strtotime( '-7 days' ) );
 		}
 
-		// Generate sample data within the date range
+		// Generate sample data within the date range.
 		$current_date = $start_date;
 		while ( $current_date <= $end_date ) {
-			$value        = rand( 100, 500 ); // Generate random value
+			$value        = wp_rand( 100, 500 ); // Generate random value.
 			$data[]       = array(
 				'date'  => $current_date,
 				'value' => $value,
 			);
-			$current_date = date( 'Y-m-d', strtotime( $current_date . ' +1 day' ) );
+			$current_date = gmdate( 'Y-m-d', strtotime( $current_date . ' +1 day' ) );
 		}
 
 		return rest_ensure_response( $data );
